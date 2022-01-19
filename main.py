7cff5db1d3d363719588550bc6bf97492ca58ab9
AlexZeGamer <alexandre.malfreyt@gmail.com>
2022-01-20 00:07:52 +0100

    modifs mineures

--- main.py
+++ main.py
@@ -28,8 +28,18 @@ async def tesla():
     channel = find(lambda c: 'tesla' in c.name, guild.text_channels)
     
     messages = await channel.history(limit=None).flatten()
-    previousListings = [int(message.embeds[0].footer.text) for message in messages]
+    
+    previousListings = [
+        int(message.embeds[0].footer.text)
+        for message in messages
+        if len(message.embeds)
+    ]
+    
     newListings = [l for l in internships if not int(l['id']) in previousListings]
+    
+    if len(newListings):
+        await channel.send('||@everyone||')
+    
     for listing in newListings:
         infosListing = getInternshipInfos(listing['id'])
         embed = disnake.Embed(
@@ -51,7 +61,7 @@ async def tesla():
         print(infosListing['title'])
 
 
-@tasks.loop(minutes=30)
+@tasks.loop(minutes=10)
 async def grades():
     grades = getGrades()
 
@@ -60,13 +70,22 @@ async def grades():
     channel = find(lambda c: 'nouvelles-notes' in c.name, guild.text_channels)
 
     messages = await channel.history(limit=None).flatten()
-    previousGrades = [message.embeds[0].footer.text for message in messages]
+    
+    previousGrades = [
+        message.embeds[0].footer.text
+        for message in messages
+        if len(message.embeds)
+    ]
+    
     newGrades = [
         grade for grade in grades
         if not f"{grade['subject-id']} - {grade['name']}" in previousGrades
     ]
     newGrades.sort(key=lambda g: g['date'])
 
+    if len(newGrades):
+        await channel.send('||@everyone||')
+    
     for grade in newGrades:
         embed = disnake.Embed(
             title=f"Nouvelle note en {grade['subject']} !",
@@ -76,10 +95,11 @@ async def grades():
             timestamp=grade['date'],
         )
 
-        embed.set_thumbnail(bot.user.avatar)
+        oasis_icon = 'https://oasis.polytech.universite-paris-saclay.fr/prod/bo/picture/favicon/polytech_paris_oasis/favicon-194x194.png'
+        embed.set_thumbnail(oasis_icon)
         embed.set_footer(text = f"{grade['subject-id']} - {grade['name']}")
 
-        await channel.send('||@everyone||', embed=embed)
+        await channel.send(embed=embed)
         print(f"{grade['subject-id']} - {grade['name']}")
         
 @bot.event
