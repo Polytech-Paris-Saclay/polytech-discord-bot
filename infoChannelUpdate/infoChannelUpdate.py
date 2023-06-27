07b7576209f3ccedc28af7a1fdc0d4f07fc08cf7
FlorianMiceli <florian.miceli@universite-paris-saclay.fr>
2023-06-27 14:41:22 +0200

    info channel

--- infoChannelUpdate/infoChannelUpdate.py
+++ infoChannelUpdate/infoChannelUpdate.py
@@ -17,22 +17,20 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command(name='updateInfo')
 async def update_chapters(ctx, channel: discord.TextChannel):
-    # Read the markdown file and parse it to extract the chapter parts
-    with open('infoChannel.md', 'r') as f:
+    os.chdir('infoChannelUpdate')
+    with open('infoChannel.md', 'r', encoding='utf-8') as f:
         markdown_file = f.read()
-    # Split the markdown text into sections based on the "##" header
-    sections = re.split(r'\n##\s+', markdown_file)[1:]
-    # Extract the title and description from each section
+    sections = re.split(r'\n##\s+', markdown_file)
     chapter_parts = []
     for section in sections:
         title, description = section.split('\n', 1)
+        #remove the ## before the title
+        title = title[2:] if title[0] == '#' else title
         chapter_parts.append({'title': title, 'description': description})
-    # Get the channel to send the embeds to
     channel = bot.get_channel(1122874143596089354)
-    # For each chapter part, create an embed with the relevant information
+    await channel.purge()
     for part in chapter_parts:
         embed = discord.Embed(title=part['title'], description=part['description'],color=0x00A8E8)
-        # Send the embed to the specified channel
         await channel.send(embed=embed)
 
 
